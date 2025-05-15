7f348e5dba4c787cbb0622002068ff620b80c474
N4647 <nitinyadav4637@gmail.com>
2025-05-15 21:56:14 +0530

    created databases branch

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import CustomUser
+from .models import CustomUser, Classrooms, Assignments
 
 class userRegisterationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -18,3 +18,13 @@ class userRegisterationSerializer(serializers.ModelSerializer):
             isStudent=validated_data['isStudent'] 
         )
         return user
+    
+class ClassroomsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Classrooms
+        fields = ['class_id', 'name', 'created_at', 'students']
+
+class AssignmentsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Assignments
+        fields = ['ass_id', 'name', 'class_id']        
