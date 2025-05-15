87f4706d23c92d5a1e3439f7181493401d83a631
N4647 <nitinyadav4637@gmail.com>
2025-05-16 00:29:08 +0530

    feat: improved models, added serializers

--- backend/users/models.py
+++ backend/users/models.py
@@ -19,7 +19,7 @@ class CustomUserManager(BaseUserManager):
         return user
     def create_superuser(self, email, username, password=None, firstName=None, lastName=None, isStudent=False):
         user = self.create_user(
-            email,
+            email=email,
             password=password,
             username=username,
             firstName=firstName,
@@ -29,7 +29,6 @@ class CustomUserManager(BaseUserManager):
         user.is_staff = True
         user.save()
         return user
-    
 
 class CustomUser(AbstractBaseUser):
     email = models.EmailField(unique=True)
@@ -62,21 +61,43 @@ class CustomUser(AbstractBaseUser):
         return self.username    
     objects = CustomUserManager()
 
+class Teacher(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.firstName} {self.user.lastName} - ({self.user.email})"
+
+class Student(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.firstName} {self.user.lastName} - ({self.user.email})"
+
 class Classrooms(models.Model):
     class_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=111)
     created_at = models.DateTimeField(auto_now_add=True)
-    students = models.ManyToManyField(CustomUser)
+    students = models.ManyToManyField(Student, related_name = "classrooms")
+    teachers = models.ManyToManyField(Teacher, related_name = "classrooms")
+
     def __str__(self):
-        return self.class_id
+        return f"Classroom {self.name} (ID: {self.class_id})"
     
 class Assignments(models.Model):
     ass_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=111)
-    class_id = models.ForeignKey('Classrooms', on_delete=models.CASCADE, related_name = 'assignments')
+    class_id = models.ForeignKey('Classrooms', on_delete = models.CASCADE, related_name = 'assignments')
+    total_Marks = models.IntegerField(default = 0)
 
     def __str__(self):
-        return self.ass_id
+        return f"Assignment {self.name} (ID: {self.ass_id})"
 
- 
-# Create your models here.
+class Submission(models.Model):
+    student = models.ForeignKey(Student, on_delete = models.CASCADE, related_name = 'submissions')
+    assignment = models.ForeignKey(Assignments, on_delete = models.CASCADE, related_name = 'submissions')
+    marks = models.IntegerField(default=0)
+    
+
+    def __str__(self):
+        return f"{self.student.user.firstName} - {self.assignment.name} - Marks: {self.marks}"
+
